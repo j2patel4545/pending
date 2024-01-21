8d112afc75bc85351a89beb4bbbd6cd56fa0a74c
jetal kalpanaben satishbhai patel <145288109+j2patel4545@users.noreply.github.com>
2024-01-21 17:43:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function videoconAnimation() {
+    var videocon = document.querySelector("#video-containter")
+    var playbtn = document.querySelector("#play")
+    videocon.addEventListener("mouseenter",function(){
+     gsap.to(playbtn,{
+        scale : 1,
+        opacity : 1
+     })
+    })
+    
+    videocon.addEventListener("mouseleave",function(){
+        gsap.to(playbtn,{
+            scale : 0,
+            opacity : 0 
+         })
+    })
+    
+    videocon.addEventListener("mousemove",function(dets){
+        gsap.to(playbtn,{
+           left:dets.x-50,
+           top:dets.y-80 
+         })
+    })
+}
+
+videoconAnimation() 
+
+function loadingAnimation(){
+    
+gsap.from("#page1 h1",{
+    y:30,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    stagger:0.2
+})
+
+gsap.from("#page1 #video-containter",{
+    scale:0.9,
+    opacity:0,
+    delay:1.3,
+    duration:0.3,
+})
+}
+loadingAnimation()
+
+document.addEventListener("mousemove", function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+    })
+
+})
